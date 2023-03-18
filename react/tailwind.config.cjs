84c3ec528b764963d17b2caea6e06afd2ac29557
Chrissa Agujar <55868590+cagujar@users.noreply.github.com>
2023-03-18 14:46:41 +0800

    added login ui

--- react/tailwind.config.cjs
+++ react/tailwind.config.cjs
@@ -2,10 +2,12 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")
+  ],
 }
